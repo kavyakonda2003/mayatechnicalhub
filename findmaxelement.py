638e6147537a6bf0a3a722bb85baba94839eeb71
22MH1A42A4 <143516809+kavyakonda2003@users.noreply.github.com>
2025-02-08 22:05:34 +0530

    Create findmaxelement.py

---
+++ findmaxelement.py
@@ -0,0 +1,6 @@
+def find_max_element():
+    N = int(input())  # Read the length of the array
+    arr = list(map(int, input().split()))  # Read the array elements
+    print(max(arr))  # Print the maximum element
+
+find_max_element()
